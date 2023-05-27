760bb859190fc8a002dbbd9f309b01da22668279
Adityya Kaushal <adityyakaushal@gmail.com>
2023-05-27 17:53:21 -0400

    Home page created!

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,30 @@
 import { Canvas } from '@react-three/fiber';
 import TorusParticle from '../assets/3D/TorusParticle';
+import Box from '../assets/3D/Box';
 import Navbar from '../components/Navbar';
 import { OrbitControls } from '@react-three/drei';
 import "../styles/Home.css"
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <div className="main">
-        <Canvas className="canvas">
+      <div className="mainHero">
+        <Navbar />
+        <TypeAnimation className="animatedText" sequence={["Math?", 1000, "Programming?", 1000, "Math and Computing Club", 2000]} wrapper="span" cursor={true} repeat={Infinity} />
+        <p className="description"> Turner's Exclusive Math and Programming Club </p>
+        <button className="btn-grad">Learn more</button>
+      </div>
+      <div className="canvas">
+        <Canvas>
           <OrbitControls enableZoom={false} />
           <ambientLight intensity={0.5} />
           <directionalLight position={[-2,5,2]} intensity={1} />
           <TorusParticle />
         </Canvas>
+      </div>
+      <div className="section2">
+        Heh
       </div>
     </>
   );
